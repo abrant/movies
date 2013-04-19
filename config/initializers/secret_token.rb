e5b2040c918e42daabb5fb25b3c480e5265496d1
abrant <abrant@csu.fullerton.edu>
2013-04-19 02:53:51 -0700

    Project Creation
    Rails project created named "movies"

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Movies::Application.config.secret_token = '999515f5371a27ab4dba11a310970b43523993e5df2f59927f7b30bac0ae14d4afbd9870e26c18bdbf3508a6f66d3a72536f72e480a3b101a293c9c25160b6c8'
